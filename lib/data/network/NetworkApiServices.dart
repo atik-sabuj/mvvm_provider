edf1e73459fc6d110226949b39f62492cb56a907
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-01 13:46:35 +0600

    Network Api Services

--- lib/data/network/NetworkApiServices.dart
+++ lib/data/network/NetworkApiServices.dart
@@ -1,2 +1,27 @@
 
-class NetworkApiService extends
+import 'dart:io';
+import 'package:mvvm_provider/data/app_exceptions.dart';
+import 'package:mvvm_provider/data/network/BaseApiServices.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiService extends BaseApiServices {
+
+  @override
+  Future getGetApiResponse(String url) async {
+
+    try {
+      final response = await http.get(Uri.parse(uri)).timeout(Duration(seconds: 10));
+    }on SocketException {
+
+      throw FetchDataException('No Internet Connection');
+    }
+
+  }
+
+  @override
+  Future getPostApiResponse(String url) {
+    // TODO: implement getPostApiResponse
+    throw UnimplementedError();
+  }
+
+}
