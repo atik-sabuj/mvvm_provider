9df4587247b32fde666dab6d4d75f59e89a1b3a2
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-01 12:08:47 +0600

    App Exception

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,21 @@
+class AppException implements Exception {
+
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  String toSting(){
+    return '&_prefix$_message';
+  }
+
+}
+
+class FetchDataException extends AppException {
+  FetchDataException([String? message]) : super(message, 'Error During Communication');
+}
+
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, 'Error During Communication');
+}
