6c8ea933e764856628d8d2352cdb9cca1a44a201
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-02 12:34:30 +0600

    Utils #3

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,24 @@
+import 'package:another_flushbar/flushbar_route.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:another_flushbar/flushbar.dart';
 
 class Utils {
 
   static toastMessage(String message){
     Fluttertoast.showToast(
         msg: message,
-    backgroundColor: Colors.red,
-    textColor: Colors.green
+    backgroundColor: Colors.black,
+
+    );
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context){
+
+    showFlushbar(context: context,
+    flushbar : Flushbar(
+      message : message,
+    )
     );
   }
 
