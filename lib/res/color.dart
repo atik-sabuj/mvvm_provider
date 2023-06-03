2c2a04f6822cec0c5a33063d1108f6154836b2a0
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-03 12:44:50 +0600

    Login View & View Model #7

---
+++ lib/res/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class AppColors {
+
+  static const Color blackColor = Color(0xFF00000);
+  static const Color whiteColor = Color(0xFFFFFFF);
+
+}
