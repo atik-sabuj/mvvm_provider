c206ad6756133812c60eadd405d362bd73972bfa
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-04 20:57:16 +0600

    Login View & View Model #13

--- lib/respository/auth_repository.dart
+++ lib/respository/auth_repository.dart
@@ -1,8 +1,32 @@
 import 'package:mvvm_provider/data/network/BaseApiServices.dart';
 import 'package:mvvm_provider/data/network/NetworkApiServices.dart';
+import 'package:mvvm_provider/res/app_url.dart';
 
 class AuthRepository {
 
   BaseApiServices _apiServices = NetworkApiService();
+
+
+  Future<dynamic> loginApi(dynamic data)async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.loginEndPoint, data);
+      return response;
+    }catch(e){
+      throw e;
+    }
+  }
+
+  Future<dynamic> registerApi(dynamic data)async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.registerApiEndPoint, data);
+      return response;
+    }catch(e){
+      throw e;
+    }
+  }
 
 }
