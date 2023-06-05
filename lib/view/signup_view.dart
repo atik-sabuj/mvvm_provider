36e26de42c052cb3e44eddf8232302b7f203e7d2
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-05 12:37:15 +0600

    Sign Up View & View Model #2

--- lib/view/signup_view.dart
+++ lib/view/signup_view.dart
@@ -8,8 +8,121 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+
+    ValueNotifier<bool> _obsecurePassword = ValueNotifier<bool>(true);
+
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    FocusNode emailFocusNode = FocusNode();
+    FocusNode passwordFocusNode = FocusNode();
+
+    @override
+    void dispose() {
+      // TODO: implement dispose
+      super.dispose();
+
+      _emailController.dispose();
+      _passwordController.dispose();
+
+      emailFocusNode.dispose();
+      passwordFocusNode.dispose();
+
+      _obsecurePassword.dispose();
+
+    }
+
+    @override
+    Widget build(BuildContext context) {
+
+      final authViewModel = Provider.of<AuthViewModel>(context);
+
+      final height = MediaQuery.of(context).size.height * 1;
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Log In'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                focusNode: emailFocusNode,
+                decoration: const InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.alternate_email)
+                ),
+                onFieldSubmitted: (value){
+                  Utils.fieldFocusChange(context, emailFocusNode, passwordFocusNode);
+                },
+              ),
+
+              ValueListenableBuilder(
+                  valueListenable: _obsecurePassword,
+                  builder: (context, value, child) {
+                    return TextFormField(
+                      controller: _passwordController,
+                      obscureText: _obsecurePassword.value,
+                      focusNode: passwordFocusNode,
+                      obscuringCharacter: "*",
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock_open_rounded),
+                        suffixIcon: InkWell(
+                            onTap: (){
+                              _obsecurePassword.value = !_obsecurePassword.value;
+                            },
+                            child: Icon(
+                                _obsecurePassword.value ? Icons.visibility_off_outlined :
+                                Icons.visibility
+                            )),
+                      ),
+                    );
+                  }
+              ),
+
+              SizedBox(height: height * .085,),
+              RoundButton(
+                title: 'Login',
+                loading: authViewModel.loading,
+                onPress: (){
+                  if(_emailController.text.isEmpty){
+                    Utils.flushBarErrorMessage('Please Enter Valid Email', context);
+                    //Utils.snackBar('Please Enter Valid Email', context);
+                    //Utils.toastMessage('Please Enter Valid Email');
+
+                  }else if(_passwordController.text.isEmpty){
+                    Utils.flushBarErrorMessage('Please Enter Correct Password', context);
+
+                  }else if(_passwordController.text.length < 6){
+                    Utils.flushBarErrorMessage('Please Enter 6 Digit Password', context);
+
+                  }else {
+                    Map data = {
+                      'email' : _emailController.text.toString(),
+                      'password' : _passwordController.text.toString(),
+                    };
+
+                    authViewModel.loginApi(data, context);
+                    print('Api Hit');
+                  }
+                },
+              ),
+              SizedBox(height: height * .02,),
+              InkWell(
+
+                  child: Text("Don't Have an Account? Sign Up"))
+            ],
+          ),
+        ),
+      );
+    }
   }
-}
+
