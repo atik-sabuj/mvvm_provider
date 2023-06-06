62583d38357e50ae905e726d321a8b70b7358ba9
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-06 11:11:41 +0600

    User Session #6

---
+++ lib/view/splash_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Splash Screen'),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
